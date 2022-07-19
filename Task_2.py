cd294a80b1f07b3e32f686a2802aa26411f62973
Александр <103884694+Vorobeey@users.noreply.github.com>
2022-07-19 19:05:16 +0300

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -2,9 +2,9 @@
 Объяснить плюсы и минусы каждой реализации.
 Первый класс - простое представление работы метода deque модуля collections. Совершает как операцию append за O(1), так и операцию popleft.
 Второй класс - примитивный вид циклического буфера. append в этом случае совершается за O(1), а pop за O(n) (n - количество элементов в массиве, 
-так как ему необходимо будет "сдвигать" элементы влево.
+так как ему необходимо будет "сдвигать" элементы влево).
+P.S. Учился с версии 3.x, не уверен, что все верно написано для версии 2.7
 '''
-
 
 import collections
 
@@ -14,7 +14,7 @@ class CircularBuffer_1(object):
     def __init__(self, maximum_el = 5):
         self.q = collections.deque(maxlen = maximum_el)
     
-    def app(self, item):
+    def append(self, item):
         self.q.append(item)
         return self.q
     
@@ -22,7 +22,30 @@ class CircularBuffer_1(object):
         self.q.popleft()
         return self.q
     
-    def pr(self):
-        return self.q
+    def prnt(self):
+        print self.q
       
+
+class CircularBuffer_2(object):
+    def __init__(self, data = [], maximum_el=5):
+        self.data = data
+        self.max = max_size
+        
+    def append(self, item):
+        if len(self.data) < self.max:
+            self.data.append(item)
+            print self.data
+            return self.data
+        else:
+            self.data.pop(0)
+            self.data.append(item)
+            print self.data
+            return self.data
+    
+    def pop(self):
+        self.data.pop(0)
+        return self.data
+    
+    def prnt(self):
+        print self.data
 
