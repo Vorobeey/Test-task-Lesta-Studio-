969ea496c167aa7e0d9194d13ce341b3e39e69a6
Александр <103884694+Vorobeey@users.noreply.github.com>
2022-07-19 19:19:46 +0300

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -2,5 +2,11 @@
 который будет аналогичен нижеприведенному по функциональности, но отличен по своей сути. 
 Объяснить плюсы и минусы обеих реализаций '''
 
-def even_num(number):
+def even_num1(number):
     return number % 2 == 0
+
+
+def even_num2(number):
+    return True if x & 1 else False
+
+        
